b24467a4af42a45e57e2fe127947c416857892c8
powerconstruction <rrogers@powerconstruction.net>
2014-08-12 19:09:01 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,10 +12,10 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'mzFXf2CiSaBqyKpFRLVAwkPx4p6O0HxGC7buP1CQvODHYMaZ', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2074475143], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'RIC Temp and Humidity', // Replaces Xively logo in the header
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
